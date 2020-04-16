202a750d649c26d5497bb0e13c50f15068e04da7
Scarlet <developerscarlet@gmail.com>
2020-04-15 22:10:15 -0400

    Initial commit for Kiter App repo

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+export default class AppView{
+
+    AppHome (props: any){
+        return(
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        )
+    }
+}

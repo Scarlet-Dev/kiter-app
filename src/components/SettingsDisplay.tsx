3c5f53790b80959b662793f4ede4d9938e328ecf
Scarlet <developerscarlet@gmail.com>
2020-04-25 20:55:03 -0400

    Added SettingsDisplay

---
+++ src/components/SettingsDisplay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+export default class Settings extends React.Component{
+    state = {}
+
+    render(){
+        return(
+            <ScrollView></ScrollView>
+        )
+    }
+}

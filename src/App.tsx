053ac5659f901a0cf202ab2e3db2e6d1676b6050
Scarlet <developerscarlet@gmail.com>
2020-04-25 23:24:03 -0400

    Added AppState management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React, {useCallback} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import { StyleSheet, Text, View, AppState } from 'react-native';
 
 export default function App() {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    AppState.addEventListener("change", _handleAppStateChange);
+
+    return () =>{
+      AppState.removeEventListener('change', _handleAppStateChange);
+    }
+  })
+
+  const _handleAppStateChange = (nextAppState: any) =>{
+    if(appState.match(/inactive|background/) && nextAppState === 'active'){
+      console.log('App has come to the foreground')
+    }
+    setAppState(nextAppState)
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

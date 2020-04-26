308bc2175a94ae9d8779ce3591415d756344f180
Scarlet <developerscarlet@gmail.com>
2020-04-25 23:41:25 -0400

    Added SettingsSectionList component.

--- src/components/SettingsDisplay.tsx
+++ src/components/SettingsDisplay.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
+const SettingsSectionList = () => {
+    return(
+        <View>
+            <SectionList 
+            sections={settings}
+            renderItem={SettingsItem}
+            renderSectionHeader={ ( { section: {title} } ) => <Text>{title}</Text> }/>
+        </View>
+    )
+}
 
 export default class Settings extends React.Component{
     state = {}

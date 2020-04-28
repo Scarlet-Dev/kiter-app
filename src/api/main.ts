9c84ed6aa60e456242107e8bd74bc880e88cdfe3
Scarlet <developerscarlet@gmail.com>
2020-04-27 23:24:59 -0400

    Added app service endpoints.

---
+++ src/api/main.ts
@@ -0,0 +1,38 @@
+import {AppRegistry } from 'react-native';
+
+export class UserService{
+    getUserProfileById(){
+
+    }
+
+    updateUserProfile(){
+
+    }
+
+    getUserSettings(){
+
+    }
+
+    updateUserSettings(){
+
+    }
+
+}
+
+export class LocationService{
+    getLocationList(){
+
+    }
+
+    addLocation(){
+
+    }
+
+    deleteLocationList(locationId: number){
+
+    }
+}
+
+export class WeatherService{
+
+}
